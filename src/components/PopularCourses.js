f1a12e00b29806fd039c2c92c5dad55ac226668b
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2021-10-24 12:34:41 +0300

    fix padding page

--- src/components/PopularCourses.js
+++ src/components/PopularCourses.js
@@ -4,7 +4,7 @@ import Title from './Title';
 export default class PopularCourses extends Component {
   render() {
     return (
-      <div className="popularCourses popularCourses--column">
+      <div className="popularCourses popularCourses--column_">
 
         <Title
           value={[
