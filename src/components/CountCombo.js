8ea91dfa84df48bcae694a52b3530b5997ed89ef
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2021-12-11 01:18:10 +0200

    Fix select-full

--- src/components/CountCombo.js
+++ src/components/CountCombo.js
@@ -2,34 +2,36 @@ import React, { Component } from 'react';
 
 import InputMask from 'react-input-mask';
 import SelectFull from './SelectFull';
-import SelectImg from '../img/сount-combo/img-1.png'; 
+
+import SelectImg1 from '../img/сount-combo/img-1.png'; 
+import SelectImg2 from '../img/сount-combo/img-2.png'; 
 
 export default class CountCombo extends Component {
   render() {
 
     const SelectData = [
       {
-        img: SelectImg,
+        img: SelectImg1,
         name: 'Базовый Курс Написания Музыки',
         price: '49 900 ₽'
       },
       {
-        img: SelectImg,
+        img: SelectImg1,
         name: 'Курс Написания Музыки',
         price: '36 300 ₽'
       },
       {
-        img: SelectImg,
+        img: SelectImg1,
         name: 'Pro Курс Написания Музыки',
         price: '65 000 ₽'
       },
       {
-        img: SelectImg,
+        img: SelectImg2,
         name: 'Курс Музыки',
         price: '18 000 ₽'
       },
       {
-        img: SelectImg,
+        img: SelectImg1,
         name: 'Базовый Курс',
         price: '9 999 ₽'
       }
