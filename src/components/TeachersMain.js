d309b5d4afbc3d45088e2be11f3e4c9837259d4e
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2021-12-15 21:25:03 +0200

    Add last corrections

--- src/components/TeachersMain.js
+++ src/components/TeachersMain.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+
 import Title from './Title';
 import Img1 from '../img/teachers-main/img-1.jpg'; 
 import Img2 from '../img/teachers-main/img-2.jpg'; 
@@ -25,59 +27,63 @@ export default class TeachersMain extends Component {
 
             <div className="teachersMain__item">
               <div className="teachersMain__box">
-                <div className="teachersMain__box-img"><img src={Img1} alt="img" /></div>
+                <div className="teachersMain__box-img"><NavLink to="/teachers/detail"><img src={Img1} alt="img" /></NavLink></div>
                 <div className="teachersMain__box-cont">
-                  <span className="teachersMain__box-name">Ванчугов Андрей</span>
+                  <span className="teachersMain__box-name">MOON SUN</span>
+                  <span className="teachersMain__box-subname">Дмитрий Лунев</span>
                   <div className="teachersMain__box-text">
-                    <p>Курс предназначен для тех, кто хочет стать DJ и не обладает навыками игры на DJ оборудовании или навыки явно недостаточны для начала выступлений в ночных заведениях</p>
+                    <p>Преподаватель Базового и PRO Курсов Написания Музыки и Базового и PRO DJ Курсов</p>
                   </div>
-                  <a className="teachersMain__box-more" href="#">Подробнее</a>
+                  <NavLink className="teachersMain__box-more" to="/teachers/detail">Подробнее</NavLink>
                 </div>
               </div>
             </div>
 
             <div className="teachersMain__item">
               <div className="teachersMain__box">
-                <div className="teachersMain__box-img"><img src={Img2} alt="img" /></div>
+                <div className="teachersMain__box-img"><NavLink to="/teachers/detail"><img src={Img2} alt="img" /></NavLink></div>
                 <div className="teachersMain__box-cont">
-                  <span className="teachersMain__box-name">Ванчугов Андрей</span>
+                  <span className="teachersMain__box-name">DJ MIRONOV</span>
+                  <span className="teachersMain__box-subname">Александр Миронов</span>
                   <div className="teachersMain__box-text">
-                    <p>Курс предназначен для тех, кто хочет стать DJ и не обладает навыками игры на DJ оборудовании или навыки явно недостаточны для начала выступлений в ночных заведениях</p>
+                    <p>Преподаватель Базового и PRO DJ Курсов</p>
                   </div>
-                  <a className="teachersMain__box-more" href="#">Подробнее</a>
+                  <NavLink className="teachersMain__box-more" to="/teachers/detail">Подробнее</NavLink>
                 </div>
               </div>
             </div>
 
             <div className="teachersMain__item">
               <div className="teachersMain__box">
-                <div className="teachersMain__box-img"><img src={Img3} alt="img" /></div>
+                <div className="teachersMain__box-img"><NavLink to="/teachers/detail"><img src={Img3} alt="img" /></NavLink></div>
                 <div className="teachersMain__box-cont">
-                  <span className="teachersMain__box-name">Ванчугов Андрей</span>
+                  <span className="teachersMain__box-name">SLY</span>
+                  <span className="teachersMain__box-subname">Кирилл Рябов</span>
                   <div className="teachersMain__box-text">
-                    <p>Курс предназначен для тех, кто хочет стать DJ и не обладает навыками игры на DJ оборудовании или навыки явно недостаточны для начала выступлений в ночных заведениях</p>
+                    <p>Преподаватель Базового DJ Курса</p>
                   </div>
-                  <a className="teachersMain__box-more" href="#">Подробнее</a>
+                  <NavLink className="teachersMain__box-more" to="/teachers/detail">Подробнее</NavLink>
                 </div>
               </div>
             </div>
 
             <div className="teachersMain__item">
               <div className="teachersMain__box">
-                <div className="teachersMain__box-img"><img src={Img4} alt="img" /></div>
+                <div className="teachersMain__box-img"><NavLink to="/teachers/detail"><img src={Img4} alt="img" /></NavLink></div>
                 <div className="teachersMain__box-cont">
-                  <span className="teachersMain__box-name">Ванчугов Андрей</span>
+                  <span className="teachersMain__box-name">DJ BANG</span>
+                  <span className="teachersMain__box-subname">Константин Усольцев</span>
                   <div className="teachersMain__box-text">
-                    <p>Курс предназначен для тех, кто хочет стать DJ и не обладает навыками игры на DJ оборудовании или навыки явно недостаточны для начала выступлений в ночных заведениях</p>
+                    <p>Преподаватель Базового и PRO DJ Курсов</p>
                   </div>
-                  <a className="teachersMain__box-more" href="#">Подробнее</a>
+                  <NavLink className="teachersMain__box-more" to="/teachers/detail">Подробнее</NavLink>
                 </div>
               </div>
             </div>
 
           </div>
 
-          <div className="titleLink"><a href="#">Все преподаватели</a></div>
+          <div className="titleLink"><NavLink to="/teachers">Все преподаватели</NavLink></div>
 
         </div>
        
